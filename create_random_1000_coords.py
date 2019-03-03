367f2390cbaac6980bdbf16fd72754de63f6ccfb
Ahmad Aburizaiza - Tips for GIS <tipsforgis@gmail.com>
2019-03-03 15:16:10 -0500

    Update create_random_1000_coords.py

--- create_random_1000_coords.py
+++ create_random_1000_coords.py
@@ -1,17 +1,29 @@
 import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
 import numpy as np
 
-# create an indeces list from 1 to 1000
-idx_list = list(range(1,1001))
 # assign the extent values
-xmin, xmax, ymin, ymax = -119.5, -88.9, 34.8, 43.0
-# create a random numpy list of longitudes within xmin <> xmax range
-x_list = (xmax - xmin) * np.random.random(1000) + xmin
-# create a random numpy list of latitudes within ymin <> ymax range
-y_list = (ymax - ymin) * np.random.random(1000) + ymin
+xmin, xmax, ymin, ymax = -179, 179, -89, 89
 
+# create a random numpy list of longitudes within xmin <> xmax range
+xs = np.random.uniform(low=xmin,high=xmax,size=(1000,))
+# create a random numpy list of latitudes within ymin <> ymax range
+ys = np.random.uniform(low=ymin,high=ymax,size=(1000,))
+# numpy column_stack method creates multidimensional array from single-dimensional arrays
+arr = np.column_stack((xs, ys))
 # create a dataframe with the index list and the two numpy arrays
-random_pd = pd.DataFrame({'id':idx_list,'x':x_list,'y':y_list})
-# store the file as csv
-random_pd.to_csv('./xys.csv', index=False)
-   
+random_df = pd.DataFrame(arr,columns=['lon','lat'])
+############################
+# store the dataframe as csv
+############################
+random_df.to_csv('./data/xys.csv', index=False)
+#######################################
+# create geodf and store it as geojson
+#######################################
+# add a column of type shapely point
+random_df['point'] = random_df.apply(lambda row: Point(row['lon'],row['lat']), axis=1)
+# create a geodataframe from a dataframe
+random_gdp = gpd.GeoDataFrame(random_df, geometry='point',crs={'init': 'epsg:4326'})
+# save the geodataframe to geojson file
+random_gdp.to_file('./data/xys.geojson',driver='GeoJSON')
